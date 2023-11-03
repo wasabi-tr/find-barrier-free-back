55bdee66a7b68c1003e01b23d3aa884708d9453c
github takaryu39 <ryuta0takamiya@gmail.com>
2023-11-03 21:07:06 +0900

    wip signup関数の実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
+import { Msg } from './interfaces/auth.interface';
 @Injectable()
 export class AuthService {
   constructor(
@@ -10,7 +13,25 @@ export class AuthService {
     private readonly jwt: JwtService,
     private readonly config: ConfigService,
   ) {}
-  async signUp(dto: AuthDto) {
-    const hashed = '';
+  async signUp(dto: AuthDto): Promise<Msg> {
+    const hashed = await bcrypt.hash(dto.password, 12);
+    try {
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashedPassword: hashed,
+        },
+      });
+      return {
+        message: 'ok',
+      };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('This email is already taken');
+        }
+        throw error;
+      }
+    }
   }
 }
