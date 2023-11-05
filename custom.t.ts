a43fada5cfd7e3c0727f89675a7b479baade69bf
github takaryu39 <ryuta0takamiya@gmail.com>
2023-11-05 13:06:36 +0900

    ユーザー情報を返すえんどポイントの実装、ユーザー情報を更新するエンドポイントの実装

---
+++ custom.t.ts
@@ -0,0 +1,6 @@
+import { User } from '@prisma/client';
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: Omit<User, 'hashedPassword'>;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
+import { Request } from 'express';
+import { User } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@UseGuards(AuthGuard('jwt')) //userのエンドポイントにjwtの認証を設定することができる
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
