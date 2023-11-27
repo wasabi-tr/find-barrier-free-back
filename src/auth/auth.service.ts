f5d291b4f7929556b583b30514346372a6a14786
github takaryu39 <ryuta0takamiya@gmail.com>
2023-11-28 08:24:04 +0900

    ユーザー取得のエンドポイント実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,4 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { AuthDto } from './dto/auth.dto';
-import * as bcrypt from 'bcrypt';
-import { Prisma } from '@prisma/client';
-import { Jwt, Msg } from './interfaces/auth.interface';
+import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   getHello(): string {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,10 +3,19 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+  async getUser(dto: GetUserDto): Promise<User> {
+    console.log(dto.id);
+
+    const user = await this.prisma.user.findUnique({
+      where: { id: dto.id },
+    });
+    return user;
+  }
   async createUser(dto: CreateUserDto): Promise<User> {
     const user = await this.prisma.user.create({
       data: {
